424d61df2c35b01b7cc5b0f9ebeaa3da6656bc93
Tre Scott <tres1396@gmail.com>
2021-02-27 08:39:18 -0500

    Added time check function

--- script.js
+++ script.js
@@ -3,8 +3,20 @@ var displayCurrentDate = moment().format("dddd, MMMM Do");
 $("#currentDay").text(displayCurrentDate);
 
 // Change timeblocks based on time of day
-var timeOfDayChangeBlock
+var currentTime = moment();
+
+// 9a block
+var changeBlock9a = function(){
+    if currentTime > moment("08:59:59").format("hh:mm:ss") && <= moment("9:59:59").format("hh:mm:ss");
+    $("#timeRow9").addClass("present");
+} else if {
+    currentTime <= moment("08:59:59").format("hh:mm:ss"); 
+    $("#timeRow9").addClass("past");
+} else {
+    $("#timeRow9").addClass("future");
+}
+return;
 
 
 
-document.getElementsByClassName("col-9").classname = "present"
+// document.getElementsByClassName("col-9").classname = "present"
