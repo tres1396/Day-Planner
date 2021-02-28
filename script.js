0025801996c35a96cf4596c9b43672f26ca38d9d
Tre Scott <tres1396@gmail.com>
2021-02-27 20:00:53 -0500

    added comments to javascript

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //Using moment.js to get current date and time 
 var displayCurrentDate = moment().format("dddd, MMMM Do");
 $("#currentDay").text(displayCurrentDate);
-
+//loads input items from storage
 window.onload = function() { 
 document.getElementById("myInput").style.display = "none";//set textbox to not visiable
 var x = localStorage.getItem("Text");//Load storage
@@ -20,7 +20,7 @@ if(time < 12) {
 	$("timeRow9").addClass = "past";
 }
 }
-
+// Input fields
 function myFunction() 
 {  
 	var x = document.getElementById("myInput");
@@ -30,7 +30,7 @@ function myFunction()
 		x.style.display = "block";
 	} 
 }
-
+// Sets input items to local storage
 function myFunction1() 
 {
 	var x = document.getElementById("myInput").value;
